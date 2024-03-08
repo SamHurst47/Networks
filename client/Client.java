ebc8144c5f3daa386a4a53c3451eb91fad96ae31
sc222sh <119955063+SamHurst47@users.noreply.github.com>
2024-03-08 23:50:30 +0000

    Update Client.java

--- client/Client.java
+++ client/Client.java
@@ -28,7 +28,7 @@ public class Client {
                         return;
                     }
                     String fileName = args[1];
-					System.out.println("client/"+fileName);
+					out.println("client/"+fileName);
                     File file = new File(fileName);
                     if (!file.exists() || !file.isFile()) {
                         System.err.println("Error: File does not exist.");
