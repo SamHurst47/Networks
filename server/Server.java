fef1b23dc544e2a848a16e8f9c4450ed5b0d794e
sc222sh <119955063+SamHurst47@users.noreply.github.com>
2024-03-08 22:16:30 +0000

    yet

--- server/Server.java
+++ server/Server.java
@@ -1,17 +1,20 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Server {
     private static final int PORT = 9101;
-    private static final String SERVER_FILES_DIRECTORY = "serverFiles"; // Corrected directory declaration
-
+    private static final String SERVER_FILES_DIRECTORY = "/serverFiles"; // Corrected directory declaration
+    private static final String LOG_FILE = "Log.txt";
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server listening on port " + PORT + "...");
@@ -29,7 +32,7 @@ public class Server {
                 if (clientMessage != null && clientMessage.equals("LIST")) {
                     System.out.println("Received 'LIST' command from client. Sending File Names");
                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
+                    logAction(0,serverSocket);
                     if (files != null && files.length > 0) {
                         for (String file : files) {
                             out.println(file);
@@ -41,40 +44,52 @@ public class Server {
                     } 
 				} else if (clientMessage != null && clientMessage.equals("PUT")) {
     				System.out.println("Received 'PUT' command from client. Receiving file...");
-                    clientMessage = in.readLine();
-                    String filename = clientMessage;
-                    boolean filenameExists = false;
-                    if (filename != null) {
-                        for (String curfile : files) {
-                            if (curfile.equals(filename)) {
-                                filenameExists = true;
-                                break;
-                            }
-                        }
-                    }
-                    if (!filenameExists) {
-                        // Receive file content from the client
-                        StringBuilder fileContent = new StringBuilder();
-                        String line;
-                        while ((line = in.readLine()) != null && !line.equals("END")) {
-                            fileContent.append(line).append("\n");
-                        }
-                    
-                        // Write received file content to a file on the server
-                        try (PrintWriter fileWriter = new PrintWriter(new FileWriter("./serverFiles/"+filename))) {
-                            fileWriter.println(fileContent.toString());
-                            System.out.println("File received and written to the server.");
-                        } catch (IOException e) {	
-                            e.printStackTrace();
-                            System.err.println("Error writing file on server.");
-                        }
-                    } else {
-                        System.err.println("Error file already on server.");
-                    }
+                    logAction(1,serverSocket);
+					String filename = clientMessage;
+					// Receive file content from the client
+					StringBuilder fileContent = new StringBuilder();
+					String line ;
+					while ((line = in.readLine()) != null && !line.equals("END")) {
+						fileContent.append(line).append("\n");
+					}
+				
+					// Write received file content to a file on the server
+					try (PrintWriter fileWriter = new PrintWriter(new FileWriter("serverFiles/"+filename))) {
+						fileWriter.println(fileContent.toString());
+						System.out.println("File received and written to the server.");
+					} catch (IOException e) {	
+						e.printStackTrace();
+						System.err.println("Error writing file on server.");
+					}
+
                 } else {
                     System.err.println("Invalid command received from client.");
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void logAction(int type, ServerSocket clientSocket) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+            LocalDateTime timestamp = LocalDateTime.now();
+            String formattedTimestamp = timestamp.format(DateTimeFormatter.ofPattern("dd-MM-yyyy | HH:mm:ss"));
+            
+            // Getting client IP address
+            InetAddress clientAddress = clientSocket.getInetAddress();
+            String clientIP = clientAddress.getHostAddress();
+            
+            // Determine request type
+            String requestType;
+            if (type == 1) {
+                requestType = "PUT";
+            } else {
+                requestType = "LIST";
+            }
+            
+            writer.write(formattedTimestamp + "|" + clientIP + "|" + requestType);
+            writer.newLine();
         } catch (IOException e) {
             e.printStackTrace();
         }
