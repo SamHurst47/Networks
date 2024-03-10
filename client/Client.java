f050391f0e9726270bd6bdcdc4ab2f5079917091
sc222sh <119955063+SamHurst47@users.noreply.github.com>
2024-03-10 17:30:28 +0000

    Update Client.java

--- client/Client.java
+++ client/Client.java
@@ -29,7 +29,7 @@ public class Client {
             }
         } catch (IOException e) {
             // Handle IOException
-            e.printStackTrace();
+            System.err.println("Error: No sever found running on port given.");
         }
     }
 
